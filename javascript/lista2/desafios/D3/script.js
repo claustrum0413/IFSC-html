d4dbf047e0b3fa1ae58f8068b9848d73e34a0390
eduardoschuquelmattioi@gmail.com <eduardoschuquelmattioi@gmail.com>
2026-06-11 11:34:30 -0300

    desafio 3 concluido

--- javascript/lista2/desafios/D3/script.js
+++ javascript/lista2/desafios/D3/script.js
@@ -1,10 +1,31 @@
-feedback = document.getElementById("feedback");
+feedback1 = document.getElementById("feedback1");
+feedback2 = document.getElementById("feedback2");
 
-function inverter() {
+function ehConsoante(char) {
+  return /^[a-z]$/.test(char.toLowerCase()) && !/^[aeiou]$/.test(char.toLowerCase());
+}
+
+function ehVogal(char) {
+  return /^[aeiou]$/.test(char.toLowerCase());
+}
+
+function contar() {
+    consoantes = 0;
+    vogais = 0;
+
     const textoInput = document.getElementById("resposta").value;
-    const textoInvertido = textoInput.split("").reverse().join("");
-    feedback.innerText = `${textoInvertido}`;
+    const textoSeparado = textoInput.split("");
+    for (const palavra of textoSeparado) {
+        if (ehConsoante(palavra)) {
+            consoantes += 1;
+        }
+        else if (ehVogal(palavra)) {
+            vogais += 1;
+        }
+    }
+    feedback1.innerText = `Número de vogais na frase: ${vogais}`;
+    feedback2.innerText = `Número de consoantes na frase: ${consoantes}`;
 }
 
 const botao = document.getElementById("botao");
-botao.addEventListener("click", inverter);
+botao.addEventListener("click", contar);
