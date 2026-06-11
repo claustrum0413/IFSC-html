a0ffb2533d4939b106434558a70458a5cfaa1b11
eduardoschuquelmattioi@gmail.com <eduardoschuquelmattioi@gmail.com>
2026-06-11 11:48:29 -0300

    comecei o desafio 4

---
+++ javascript/lista2/desafios/D4/script.js
@@ -0,0 +1,8 @@
+const tr = document.getElementById("triangulo");
+tr.addEventListener("click", tri);
+const qd = document.getElementById("quadrado");
+qd.addEventListener("click", quad);
+
+function tri() {
+  console.log()
+}
